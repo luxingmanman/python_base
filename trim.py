b0cb8a2e5dcc63fbc9fc0bad01a977d0c420dfd4
种路路 <luxingmanman@sina.com>
2018-02-25 17:30:53 +0800

    切片操作，实现一个trim()函数

--- trim.py
+++ trim.py
@@ -1,6 +1,5 @@
-#利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法：
-
 # -*- coding: utf-8 -*-
+#利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法：
 def trim(s):
     if s[:1] == ' ' :
         return trim(s[1:])
@@ -8,8 +7,6 @@ def trim(s):
         return trim(s[:-1])
     else :
         return s
-
-# 测试:
 if trim('hello  ') != 'hello':
     print('测试失败!')
 elif trim('  hello') != 'hello':
